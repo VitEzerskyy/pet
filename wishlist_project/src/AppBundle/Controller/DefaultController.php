abd17fce5d17264f917a7e6e37840179e81a6af4
VitEzerskyy <_ezerskyy@ukr.net>
2017-04-03 18:13:48 +0300

    new

---
+++ wishlist_project/src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Template()
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        return [];
+
+    }
+
+
+}
