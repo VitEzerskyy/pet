5e115745954ca1ebed4114f9d7aecbf5b5f5d58c
VitEzerskyy <_ezerskyy@ukr.net>
2017-04-05 13:30:18 +0300

    final

--- wishlist_project/src/AppBundle/Resources/public/js/add_wish.js
+++ wishlist_project/src/AppBundle/Resources/public/js/add_wish.js
@@ -1,6 +1,5 @@
 function addItem() {
     var div = document.getElementsByClassName('default')[0];
-    console.log(div);
     div.classList.toggle('moving');
 }
     var elem = document.getElementById('add_item');
